6de24279a3409b9993fca3ba0f4f191187ca510c
Laughing Sun Development <LaughingSun@users.noreply.github.com>
2015-06-18 13:01:08 +0800

    Update make.bat

--- make.bat
+++ make.bat
@@ -1,2 +1,2 @@
-uglifyjs --compress -mangle -o ..\..\regexhelper.js .\regexhelper.js
-jsdoc -r README.md src\regexhelper\regexhelper.js -d .\docs 
+@uglifyjs --compress -mangle -o ..\..\regexhelper.js .\regexhelper.js
+@jsdoc -r README.md src\regexhelper\regexhelper.js -d .\docs 
